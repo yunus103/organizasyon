33fd94ddf7863fdc8e3781249992a437b0188403
yunus103 <yunusemre.aytekin@ogr.iuc.edu.tr>
2026-04-30 11:22:07 +0300

    Redirect

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/istanbul-dueguen-organizasyon-hizmetleri",
+        destination: "/istanbul-dugun-organizasyon-hizmetleri",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
