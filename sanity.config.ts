941f5bfce354203e9ff2144bcd07edefcf133147
yunus103 <yunusemre.aytekin@ogr.iuc.edu.tr>
2026-06-02 10:52:40 +0300

    Media added to studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,6 +7,8 @@
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+import { media } from "sanity-plugin-media";
+import { trTRLocale } from "@sanity/locale-tr-tr";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./src/sanity/env";
@@ -42,6 +44,8 @@ export default defineConfig({
             S.documentTypeListItem("blogCategory").title("Blog Kategorileri"),
           ]),
     }),
+    media(),
+    trTRLocale(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
